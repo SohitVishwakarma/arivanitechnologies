7f6f8d6678249cf353c9a44ecb2aff96c83fd35f
akash <akash@arivani.net>
2024-04-17 15:18:33 -0400

    fbgdgfeb

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Contactus = () => {
   return (
     <div>
-      Hello Contact
+      Hello Contactew bvv v iuhei vpui v hev
+      fe vovporwv ewqh1
+
+      <h1 className=' text-amber-500 font-bold text-2xl'>
+        helll0 0 90i g
+      </h1>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import { BrowserRouter, } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from "./App.jsx";
 import "./index.css";
+import Contactus from "./components/Contactus.jsx";
 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-          <App />
+    <App />
+    <Routes>
+    <Route path="/Contactus" element={<Contactus />} />
+    <Route path="/aboutus" element="/"/>
+
+
+    </Routes>
+          
       
     
     </BrowserRouter>
