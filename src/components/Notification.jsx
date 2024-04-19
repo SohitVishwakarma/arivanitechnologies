4ce8e2a5fae45849a44b8d825668d435431c3fec
akash <akash@arivani.net>
2024-04-19 15:43:58 -0400

    dsbvdfsv

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -11,19 +11,19 @@ const Notification = ({ className, title }) => {
     >
       <img
         src={notification1}
-        width={62}
+        width={62}         
         height={62}
         alt="image"
         className="rounded-xl"
       />
       <div className="flex-1">
-        <h6 className="mb-1 font-semibold text-base">{title}</h6>
+        <h6 className="mb-1 text-base font-semibold">{title}</h6>
         <div className="flex items-center justify-between">
           <ul className="flex -m-0.5">
             {notificationImages.map((item, idx) => (
               <li
                 key={idx}
-                className="flex w-6 h-6 border-2 border-n-12 rounded-full overflow-hidden"
+                className="flex w-6 h-6 overflow-hidden border-2 rounded-full border-n-12"
               >
                 <img
                   src={item}
