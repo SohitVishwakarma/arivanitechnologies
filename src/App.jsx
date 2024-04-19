df369aa161433175ec3729e95161ffee4546eb7e
akash <akash@arivani.net>
2024-04-19 15:43:48 -0400

    gfb

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-
-import Collaboration from "./components/Collaboration";
+import { Routes, Route } from 'react-router-dom';
+import Contactus from './components/Contactus'
+// import Collaboration from "./components/Collaboration";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-         <Header />
-          <Hero />
-          <Benefits />
-          {/*  <Collaboration />  */}
+      <Header />
+        <Hero />
+        <Benefits />
+         {/* <Collaboration />  */}
+        <Routes>
+        
+          <Route path="/Contactus" element={<Contactus />} />
+          <Route path="/aboutus" element="" />
+          {/* <Route path="/services"element={<Services />} /> */}
+
+
+        </Routes>
+        {/* <Services/> */}
 
       </div>
       <ButtonGradient />
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, } from 'react-router-dom';
 import App from "./App.jsx";
 import "./index.css";
-import Contactus from "./components/Contactus.jsx";
+// import Contactus from "./components/Contactus.jsx";
 import Footer from "./components/Footer";
-import Services from "./components/Services";
+// import Services from "./components/Services";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
      
-    <Routes>
-    <Route path="/Contactus" element={<Contactus />} />
-    <Route path="/aboutus" element="/"/>
-    {/* <Route path="/services"element={<Services />} /> */}
    
-
-    </Routes>
-    {/* <Services/> */}
           
     <Footer />
     
